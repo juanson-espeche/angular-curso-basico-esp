39f1f37cb6faf9987a7b750a268f36b5a1f46a16
JuanEspeche <jespeche@slingr.io>
2022-11-28 10:57:00 -0300

    fin sección 5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppComponent } from './app.component';
 
 import { HeroesMudole } from './heroes/heroes.module';
 import { ContadorModule } from './contador/contador.module';
+import { DbzModule } from './dbz/dbz.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HeroesMudole, ContadorModule],
+  imports: [BrowserModule, HeroesMudole, ContadorModule, DbzModule],
   providers: [],
   bootstrap: [AppComponent],
 })
